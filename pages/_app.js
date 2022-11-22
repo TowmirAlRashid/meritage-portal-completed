2d0a1da66a2b4365f04428810b9b9a8f72077552
TowmirAtInsta <towmir@instawebworks.com.au>
2022-11-22 09:41:34 +0600

    created boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+import { CacheProvider } from '@emotion/react';
+import BlankLayout from "../BlankLayout/BlankLayout";
+import { CssBaseline } from '@mui/material';
+import createEmotionCache from '../src/createEmotionCache'
+
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache, }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <CssBaseline />
+      <BlankLayout>
+        <Component {...pageProps} />
+      </BlankLayout>
+    </CacheProvider>
+  )
 }
 
 export default MyApp

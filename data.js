2ee688a34a46a5468a2de57a025c8e9cf61c6c60
TowmirAtInsta <towmir@instawebworks.com.au>
2022-11-24 09:33:38 +0600

    added mock data

---
+++ data.js
@@ -0,0 +1,100 @@
+const data = [
+    {
+        id: 1,
+        eng_name: "Emranul Test 01",
+        eng_date: "09 Nov 2019",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 2,
+        eng_name: "Emranul Test 02",
+        eng_date: "08 Nov 2019",
+        eng_stage: "On-Bording/Intake"
+    },
+    {
+        id: 3,
+        eng_name: "Emranul Test 03",
+        eng_date: "09 Nov 2010",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 4,
+        eng_name: "Emranul Test 04",
+        eng_date: "29 Dec 2019",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 5,
+        eng_name: "Emranul Test 05",
+        eng_date: "09 Nov 2019",
+        eng_stage: "On-Bording/Intake"
+    },
+    {
+        id: 6,
+        eng_name: "Emranul Test 06",
+        eng_date: "09 Nov 2018",
+        eng_stage: "On-Bording/Intake"
+    },
+    {
+        id: 7,
+        eng_name: "Emranul Test 07",
+        eng_date: "09 Nov 2019",
+        eng_stage: "On-Bording/Intake"
+    },
+    {
+        id: 8,
+        eng_name: "Emranul Test 08",
+        eng_date: "09 Nov 2019",
+        eng_stage: "On-Bording/Intake"
+    },
+    {
+        id: 9,
+        eng_name: "Emranul Test 09",
+        eng_date: "09 Nov 2019",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 10,
+        eng_name: "Emranul Test 10",
+        eng_date: "09 Nov 2019",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 11,
+        eng_name: "Emranul Test 11",
+        eng_date: "09 Nov 2019",
+        eng_stage: "On-Bording/Intake"
+    },
+    {
+        id: 12,
+        eng_name: "Emranul Test 12",
+        eng_date: "09 Nov 2019",
+        eng_stage: "On-Bording/Intake"
+    },
+    {
+        id: 13,
+        eng_name: "Emranul Test 13",
+        eng_date: "09 Nov 2019",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 14,
+        eng_name: "Emranul Test 14",
+        eng_date: "09 Nov 2019",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 15,
+        eng_name: "Emranul Test 15",
+        eng_date: "09 Nov 2019",
+        eng_stage: "Marketing"
+    },
+    {
+        id: 16,
+        eng_name: "Emranul Test 16",
+        eng_date: "09 Nov 2019",
+        eng_stage: "On-Bording/Intake"
+    },
+]
+
+export default data

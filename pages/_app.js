d4c25a97099da70333a516916892ba521f9a2018
TowmirAtInsta <towmir@instawebworks.com.au>
2022-11-26 11:26:37 +0600

    added context api to change value globally

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,24 @@ import BlankLayout from "../layouts/BlankLayout/BlankLayout";
 import { CssBaseline } from '@mui/material';
 import createEmotionCache from '../src/createEmotionCache'
 
+import AppContext from '../AppContext';
+import { useState } from 'react';
+
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 function MeritagePortal({ Component, pageProps, emotionCache = clientSideEmotionCache, }) {
+  const [value, setValue] = useState("Record Details");
+
   return (
     <CacheProvider value={emotionCache}>
-      <CssBaseline />
-      <BlankLayout>
-        <Component {...pageProps} />
-      </BlankLayout>
+      <AppContext.Provider value={{value, setValue}}>
+        <CssBaseline />
+        <BlankLayout>
+          <Component {...pageProps} />
+        </BlankLayout>
+      </AppContext.Provider>
     </CacheProvider>
   )
 }
